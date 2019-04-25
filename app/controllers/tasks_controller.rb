8e8d9ac9376b2744b9b50beac1688a0aa9ff004b
Jonathan Tuzman <tuzmusic@gmail.com>
2019-04-25 18:06:52 +0000

    write and pass simple index spec

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,5 @@
 class TasksController < ApplicationController
+  def index
+    render json: Task.all
+  end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
+  
+  let(:task) { Task.create(name: "Do a thing") }
+  
+  it "returns a json object for a task" do
+    task =  Task.create(name: "Do a thing") 
+    get :index
+    returned_json = response.body
+    # binding.pry
+    parsed_json = JSON.parse(returned_json)
+    expect(parsed_json[0]['name']).to eq task.name
 
+  end
 end
