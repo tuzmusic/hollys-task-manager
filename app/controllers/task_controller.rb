374414259d4c824651d52ac047af1d284a3bee79
Jonathan Tuzman <tuzmusic@gmail.com>
2019-04-25 17:39:30 +0000

    generate rspec

--- app/controllers/task_controller.rb
+++
@@ -1,2 +0,0 @@
-class TasksController < ActionController::API
-end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a name" do
+    expect(Task).to respond_to(:name)  
+  end
 end
