874579272a5e78ddef15e44f658c00ddb0da53d2
Jonathan Tuzman <tuzmusic@gmail.com>
2019-04-26 18:27:57 +0000

    refactor controller specs

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,18 +5,18 @@ describe TasksController, type: :controller do
   let(:task2) { Task.create(name: "Do this first") }
   let(:task3) { Task.create(name: "Do this second") }
 
+  before :each do
+    get :show, params: {id: task1.id}
+    @json = JSON.parse(response.body)
+  end
+
   describe "GET /tasks/:id" do    
     it "returns a json object for a task" do
-      get :show, params: {id: task1.id}
-      name = task1.name
-      parsed_json = JSON.parse(response.body)
-      expect(parsed_json['name']).to eq name
+      expect(@json['name']).to eq task1.name
     end
   
     it "includes completeness of a task" do
-      get :show, params: {id: task1.id}
-      json1 = JSON.parse(response.body)
-      expect(json1['completed']).to eq false
+      expect(@json['completed']).to eq false
     end
   
     it "shows a change in completeness of a task" do
@@ -28,9 +28,7 @@ describe TasksController, type: :controller do
     end
   
     it "shows whether a task is completable" do
-      get :show, params: {id: task1.id}
-      json = JSON.parse(response.body)
-      expect(json['completable']).to eq true
+      expect(@json['completable']).to eq true
     end  
   
     it "includes the prereq_ids" do
@@ -45,10 +43,17 @@ describe TasksController, type: :controller do
   
   describe "POST /tasks" do
     before :each do
-      task_attr = { name: task1.name, description: task1.description, prereq_ids: task1.prereq_ids, completed: task1.completed, completable: task1.completable }
+      task_attr = { name: task1.name, description: task1.description, prereq_ids: task1.prereq_ids, completed: task1.completed, completable: task1.completable? }
+      post :create, task: task_attr
     end
+
     it "creates a tasks with given params" do
-      
+      @json = JSON.parse(response.body)
+      expect(body['name']).to eq task1.name
+      expect(body['description']).to eq task1.description
+      expect(body['prereq_ids']).to eq task1.prereq_ids
+      expect(body['completed']).to eq task1.completed
+      expect(body['completable']).to eq task1.completable?
     end
   end
 end
