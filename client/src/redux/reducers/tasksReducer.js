dcd3f0860498237517283aa34637af6c7f0454f4
Jonathan Tuzman <tuzmusic@gmail.com>
2019-04-28 22:31:56 +0000

    add delete action in reducer

--- client/src/redux/reducers/tasksReducer.js
+++ client/src/redux/reducers/tasksReducer.js
@@ -5,10 +5,24 @@ export const initialState = {
     description: "Sample description",
     completed: false
   }
+};
+
+function createTask(state, { name, description }) {
+  const id =
+    Object.keys(state).reduce((maxId, id) => Math.max(id, maxId), -1) + 1;
+  return { [id]: { id, name, description, completed: false } };
 }
 
 export default function taskReducer(state = initialState, action) {
   switch (action.type) {
-    default: return state
+    case "ADD_TASK":
+      const newTask = createTask(state, action.task);
+      return { ...state, ...newTask };
+    case "DELETE_TASK":
+      const clone = { ...state };
+      delete clone[action.id];
+      return clone;
+    default:
+      return state;
   }
 }
--- client/tests/taskReducer.test.js
+++ client/tests/taskReducer.test.js
@@ -1,7 +1,38 @@
 import reducer, { initialState } from "../src/redux/reducers/tasksReducer";
 
-describe("todos reducer", () => {
+describe("tasks reducer", () => {
   it("should return the initial state", () => {
     expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it("should add a task", () => {
+    const newTask = {
+      name: "new task name",
+      description: "new task description"
+    };
+    const action = { type: "ADD_TASK", task: newTask };
+
+    const expectedState = {
+      ...initialState,
+      2: { ...newTask, id: 2, completed: false }
+    };
+
+    expect(reducer(initialState, action)).toEqual(expectedState);
+  });
+
+  it("should delete a task", () => {
+    const action = { type: "DELETE_TASK", id: 2 };
+    const newTask = {
+      name: "new task name",
+      description: "new task description",
+      id: 2,
+      completed: false
+    };
+    const newState = {
+      ...initialState,
+      2: { ...newTask }
+    };
+
+    expect(reducer(newState, action)).toEqual(initialState);
   });
 });
