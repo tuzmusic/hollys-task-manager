f5f95377cb0c6c159d9860d74642e7c1dfb9c48a
Jonathan Tuzman <tuzmusic@gmail.com>
2019-04-28 21:09:44 +0000

    add edit and delete actions

--- client/src/redux/actions/tasksActions.js
+++ client/src/redux/actions/tasksActions.js
@@ -5,3 +5,25 @@ export function addTask(task) {
 export function toggleTask(task) {
   return {type: "EDIT_TASK", id: task.id, key: 'completed', value: !task.completed }
 }
+
+export function editTaskName(task, name) {
+  return {
+    type: "EDIT_TASK",
+    id: task.id,
+    key: "name",
+    value: name
+  }; 
+}
+
+export function editTaskDescription(task, description) {
+  return {
+    type: "EDIT_TASK",
+    id: task.id,
+    key: "description",
+    value: description
+  }; 
+}
+
+export function deleteTask(task) {
+  return {type: 'DELETE_TASK', id: task.id}
+}
---
+++ client/tests/taskActions.test.js
@@ -0,0 +1,65 @@
+import * as actions from "../src/redux/actions/tasksActions";
+
+describe("addTask", () => {
+  it("should create an action to add a task", () => {
+    const task = { name: "Finish docs" };
+    const expectedAction = { type: "ADD_TASK", task };
+    expect(actions.addTask(task)).toEqual(expectedAction);
+  });
+});
+
+describe("toggleTask", () => {
+  it("should return a task id and completeness for the task", () => {
+    const task = { id: 1, name: "Finish docs", completed: false };
+    const expectedAction = {
+      type: "EDIT_TASK",
+      id: 1,
+      key: "completed",
+      value: true
+    };
+    expect(actions.toggleTask(task)).toEqual(expectedAction);
+  });
+});
+
+describe('editTaskName', () => {
+  it("should return a task id and new name for the task", () => {
+    const task = { id: 1, name: "Finish docs" };
+    const newName = "Do this instead"
+    const expectedAction = {
+      type: "EDIT_TASK",
+      id: 1,
+      key: "name",
+      value: newName
+    };
+    expect(actions.editTaskName(task, newName)).toEqual(expectedAction);
+  });
+})
+
+describe('editTaskDescription', () => {
+  it("should return a task id and new name for the task", () => {
+    const task = { id: 1, name: "Finish docs" };
+    const newDescription = "Do this instead"
+    const expectedAction = {
+      type: "EDIT_TASK",
+      id: 1,
+      key: "description",
+      value: newDescription
+    };
+    expect(actions.editTaskDescription(task, newDescription)).toEqual(
+      expectedAction
+    );
+  });
+})
+
+describe('deleteTask', () => {
+  it("should return delete action a task id", () => {
+    const task = { id: 1, name: "Finish docs" };
+    const expectedAction = {
+      type: "DELETE_TASK",
+      id: 1,
+    };
+    expect(actions.deleteTask(task)).toEqual(
+      expectedAction
+    );
+  });
+})
