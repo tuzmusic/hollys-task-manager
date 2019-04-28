6e200e78806a586221d61f918f0dca7df418c8c9
Jonathan Tuzman <tuzmusic@gmail.com>
2019-04-28 18:31:02 +0000

    add testing packages and config

--- client/src/redux/tasksActions.js
+++ client/src/redux/tasksActions.js
@@ -1,13 +1,3 @@
-const fetch = require("node-fetch");
-
-
-function getTasks() {
-  const indexURL = 'http://localhost:3001/tasks'
-  fetch(indexURL)
-  .then(res => res.json())
-  .then(json => {
-    console.log(json);
-  });
+export function addTask(task) {
+ return { type: 'ADD_TASK', task} 
 }
-
-getTasks()
