bd83337ef696d67bff07bc9d125277ef84ad5c99
Jonathan Tuzman <tuzmusic@gmail.com>
2019-04-30 14:51:38 +0000

    wrap app in Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import "./App.css";
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+
 import TaskComponent from "./components/TaskComponent";
 import Task from "./models/Task";
+import taskReducer from "./redux/reducers/tasksReducer";
+
+const reducer = combineReducers({ tasks: taskReducer });
+const store = createStore(reducer, {});
 
 function App() {
-  const someTask = new Task ({ name: "Sample task!" });
-  console.log(someTask);
-  
+  const someTask = new Task({ name: "Sample task!" });
+
   return (
-    <div className="App">
-      <TaskComponent task={someTask} />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <TaskComponent task={someTask} />
+      </div>
+    </Provider>
   );
 }
 
